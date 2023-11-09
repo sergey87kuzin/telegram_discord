39d55be5361d0e8369360923060a6b2cfda46070
Sergei Kuzin <s.kuzin@stik.pro>
2023-11-09 09:38:09 +0300

    celery beat

--- CI/entrypoint.sh
+++ CI/entrypoint.sh
@@ -8,6 +8,6 @@ python /code/manage.py collectstatic --no-input
 celery --app=telegram_to_discord worker -l info --logfile=celery.log -Q default -n default &
 celery --app=telegram_to_discord beat -l info --logfile=celery_beat.log &
 
-celery --broker=redis://redis:6379 -A telegram_to_discord flower --address=web --port=5555 --url_prefix=flower &
+celery -A telegram_to_discord flower --address=web --port=5555 --url_prefix=flower &
 
 gunicorn telegram_to_discord.wsgi:application --bind 0.0.0.0:8000
--- telegram_to_discord/celerybeat.py
+++ telegram_to_discord/celerybeat.py
@@ -1,9 +1,16 @@
+from datetime import timedelta
+
 from celery.schedules import crontab
 
 CELERY_BEAT_SCHEDULE = {
     "check_discord_answers": {
         "task": "discord_messages.tasks.get_discord_messages",
-        "schedule": crontab(minute="*/1"),
-        "options": {"queue": "messages"},
+        "schedule": timedelta(seconds=29),
+        "options": {"queue": "telegram"},
+    },
+    "send_discord_answers": {
+        "task": "discord_messages.tasks.send_messages_to_telegram",
+        "schedule": timedelta(seconds=31),
+        "options": {"queue": "telegram"},
     },
 }
