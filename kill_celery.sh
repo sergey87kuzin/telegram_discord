dfbb3939223231cf59d6ecb129c0d084036fc624
Sergei Kuzin <s.kuzin@stik.pro>
2023-12-09 23:29:10 +0300

    celery queues

--- kill_celery.sh
+++ kill_celery.sh
@@ -1,1 +1,2 @@
 kill -9 $(ps aux | grep celery | grep -v grep | awk '{print $2}' | tr '\n' ' ') > /dev/null 2>&1
+sudo celery multi stop 2 -A telegram_to_discord -c 1 -B:2 -Q:1 messages -Q:2 telegram
--- restart_celery.sh
+++ restart_celery.sh
@@ -1,7 +1,6 @@
 #pkill -9 celery
 
-celery --app=telegram_to_discord worker -l warning --logfile=celery.log --max-tasks-per-child=1 --concurrency=1 --without-heartbeat -Q telegram -n telegram &
-celery --app=telegram_to_discord worker -l warning --logfile=celery.log --concurrency=1 --without-heartbeat -Q messages -n messages &
+sudo celery multi start 2 -A telegram_to_discord -c 1 -B:2 -Q:1 messages -Q:2 telegram
 celery --app=telegram_to_discord beat -l warning --logfile=celery_beat.log &
 
 celery -A telegram_to_discord flower --address=localhost --port=5555 --url_prefix=flower &
