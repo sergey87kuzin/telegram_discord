081ffa09a8393ec41e2a805128d55b175a0ef228
Mona Lisa <s.kuzin@stik.pro>
2023-12-24 15:09:07 +0300

    zoom settings

--- restart_celery.sh
+++ restart_celery.sh
@@ -1,6 +1,6 @@
 #pkill -9 celery
-
-sudo celery multi start 2 -A telegram_to_discord -c 1 -B:2 -Q:1 messages -Q:2 telegram
+source venv/bin/activate
+celery multi start 2 -A telegram_to_discord -c 1 -B:2 -Q:1 messages -Q:2 telegram
 celery --app=telegram_to_discord beat -l warning --logfile=celery_beat.log &
 
 celery -A telegram_to_discord flower --address=localhost --port=5555 --url_prefix=flower &
