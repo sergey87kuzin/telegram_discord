dc6297811f5305ff7de80a4d2c249d664bf7aaf7
Mona Lisa <s.kuzin@stik.pro>
2023-12-24 20:12:24 +0300

    zoom scales

--- stable_messages/tasks.py
+++ stable_messages/tasks.py
@@ -124,7 +124,7 @@ def send_zoom_to_stable(created_message_id, direction):
         "num_inference_steps": 20,
         "as_video": "no",
         # "num_interpolation_steps": 32,
-        "walk_type": [direction] * 8,
+        "walk_type": [direction] * 16,
         "track_id": stable_message.id,
         "webhook": settings.SITE_DOMAIN + reverse_lazy("stable_messages:stable-webhook"),
     })
