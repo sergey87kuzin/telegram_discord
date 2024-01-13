25a8cfd14b1ec0a42048777f12305cd5f17f3127
greyquattro <s.kuzin@stik.pro>
2024-01-13 11:25:38 +0300

    custom_settings

--- users/models.py
+++ users/models.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 from discord_messages.models import DiscordAccount
+from stable_messages.choices import StableModels
 from users.managers import CustomUserManager
 
 
@@ -17,6 +18,37 @@ class Style(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class CustomSettings(models.Model):
+    model_id = models.CharField(
+        "Номер модели",
+        max_length=256,
+        blank=True,
+        null=True,
+        # choices=StableModels.CHOICES,
+        default=StableModels.JUGGERNAUT
+    )
+    seed = models.CharField("Сид", max_length=128, blank=True, null=True, default="-1")
+    num_inference_steps = models.CharField("Число шагов", max_length=8, blank=True, null=True, default="20")
+    guidance_scale = models.PositiveIntegerField("Шкала", default=7)
+    embeddings_model = models.CharField("Связанная модель", max_length=256, blank=True, null=True)
+    negative_prompt = models.CharField("Негативный промпт", max_length=2048, blank=True, null=True)
+    positive_prompt = models.CharField("Позитивный промпт", max_length=2048, blank=True, null=True)
+    lora_model = models.CharField("Модель Лора", max_length=256, blank=True, null=True)
+    lora_strength = models.CharField("lora strength", max_length=128, blank=True, null=True)
+    sampling_method = models.CharField("Метод семплинга", max_length=256, blank=True, null=True)
+    algorithm_type = models.CharField("Тип алгоритма", max_length=256, blank=True, null=True)
+    scheduler = models.CharField("scheduler", max_length=256, blank=True, null=True)
+    vary_num_inference_steps = models.CharField("Вариации число шагов", max_length=16, blank=True, null=True)
+    vary_guidance_scale = models.FloatField("Вариации шкала(7.5)", blank=True, null=True)
+    vary_strength = models.FloatField("Вариации сила картинки", blank=True, null=True)
+    controlnet_model = models.CharField("controlnet_model", max_length=256, blank=True, null=True)
+    controlnet_type = models.CharField("controlnet_type", max_length=256, blank=True, null=True)
+
+    class Meta:
+        verbose_name = "Кастомные настройки для пользователя"
+        verbose_name_plural = verbose_name
 
 
 class User(AbstractUser):
@@ -58,6 +90,7 @@ class User(AbstractUser):
         blank=True,
         null=True
     )
+    custom_settings = models.ForeignKey(CustomSettings, on_delete=models.SET_NULL, blank=True, null=True)
     USERNAME_FIELD = "username"
     REQUIRED_FIELDS = []
     objects = CustomUserManager()
