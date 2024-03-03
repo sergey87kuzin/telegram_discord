1edd35a4318241ea900f697a834894673b877bb4
greyquattro <s.kuzin@stik.pro>
2024-03-03 20:18:39 +0300

    courses prolongations

--- courses/admin.py
+++ courses/admin.py
@@ -2,7 +2,7 @@ from ckeditor_uploader.widgets import CKEditorUploadingWidget
 from django import forms
 from django.contrib import admin
 
-from courses.models import Course, Lesson, UserCourses, LessonTextBlock, Review
+from courses.models import Course, Lesson, UserCourses, LessonTextBlock, Review, Prolongation
 
 
 class LessonTextBlockAdminForm(forms.ModelForm):
@@ -26,6 +26,11 @@ class LessonTextBlockAdmin(admin.ModelAdmin):
 
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Prolongation)
+class ProlongationAdmin(admin.ModelAdmin):
     pass
 
 
