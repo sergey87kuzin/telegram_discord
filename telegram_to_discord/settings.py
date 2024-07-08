58c05a022436a6a967196df6683ef1da85f4cc19
greyquattro <s.kuzin@stik.pro>
2024-07-08 14:44:05 +0300

    static files

--- telegram_to_discord/settings.py
+++ telegram_to_discord/settings.py
@@ -172,13 +172,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 MEDIA_ROOT = str(BASE_DIR / "media")
-if not DEBUG:
+if DEBUG:
     STATIC_ROOT = str(BASE_DIR / "staticfiles")
 else:
     STATIC_ROOT = "/var/html/static/"
 MEDIA_URL = "/media/"
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [str(BASE_DIR / "static")]
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    "/var/html/static/",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
